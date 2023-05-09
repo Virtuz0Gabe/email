04e95a9e24019ee3c4420940a3bdcc7665f3ee64
Virtuz0Gabe <117865586+Virtuz0Gabe@users.noreply.github.com>
2023-05-09 17:48:55 -0300

    Add files via upload

---
+++ composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gabe/email',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6501bb6466e4728e09c26cad8b196b3aed51c50b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gabe/email' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6501bb6466e4728e09c26cad8b196b3aed51c50b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
